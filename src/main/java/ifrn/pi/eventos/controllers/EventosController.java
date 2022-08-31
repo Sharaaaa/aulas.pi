1bf9fac46efac0eb35490f3c9bfb0f7e95d51ef5
Shara Serafim <shara.i@escolar.ifrn.edu.br>
2022-08-31 17:28:42 -0300

    Criando a página de formulário para cadastro do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,6 +1,7 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class EventosController {
